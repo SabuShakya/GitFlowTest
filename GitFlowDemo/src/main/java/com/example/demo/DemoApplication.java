e7679e5453c76ebf4ca9c737c1746cc7c6abead7
Bijay Shrestha <bijay.shrestha@f1soft.com>
2018-06-01 12:52:03 +0545

    hell obijay

--- GitFlowDemo/src/main/java/com/example/demo/DemoApplication.java
+++ GitFlowDemo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,5 +8,16 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("this is done by bijay shresthqa");
 	}
+
+	public void bijay()
+	{
+		System.out.println("hello bijay");
+	}
+
+	//helloo worle
 }
+
+
